c4c4dfa7cbad8a75b239c404cb725bb24e44036e
Mateen Kazia <mateencode@gmail.com>
2019-05-21 11:17:48 -0700

    boiler plate setting up

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import SearchBar from "./SearchBar";
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
